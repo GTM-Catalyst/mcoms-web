b5daed571efb4653996649cfbe79559839192e01
Nishit Kumar <nisitkumar@gmail.com>
2020-09-03 15:57:31 +0530

    share with Abhishek 3rd September

---
+++ docs/chap4.3_Dashboard.md
@@ -0,0 +1,17 @@
+---
+id: dash
+title: Dashboard
+sidebar_label: Dashboard
+---
+
+**Functionality**
+* The Dashboard is the homes screen of the app.
+* It lays out the various functionalities that can be used by the customer; these are discussed below.
+
+
+**Page Details**
+
+![Dashboard](./assets/4.8_Dashboard.png) 
+
+
+
---
+++ sidebars.js
@@ -0,0 +1,9 @@
+module.exports = {
+  someSidebar: {  
+    'Introduction': ['opchallenge','roleofcom', 'IntSysInt', 'usermanual'],
+    'Operations and Their Workflows': ['IntroOpActivity','CustRelOp', 'TechOp', 'SiteFinRprtng', 'DataVerSteps'],
+    'Field Agent App': ['introduction','InstallSetup', 'HomScr', 'MainMenu', 'consumers','PckAndSchm','plant','meter', 'water', 'mislns', 'faq'],
+    'Customer App': ['IntroCust','InstallSetup4', 'dash', 'MainMenuCust', 'profile','AmPbl','BllPyDet','PkgChngRq', 'SvrcRqst', 'Ntfctn', 'SchmAndOffrs', 'faqcust'],
+    'APPENDICES': ['glssry'],
+  },
+};
